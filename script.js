558055a598ab2043cb323fe7db596c1ee67e8be2
Sajith1017 <sajithahmed1017@gmail.com>
2024-07-07 12:58:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var a = document.getElementById("image")
+function fanon(){
+    a.style.animationDuration=3+"s"
+}
+function fanoff(){
+    a.style.animationDuration=0+"s"
+}
+function fanon1(){
+    a.style.animationDuration=1+"s"
+}
+function fanon2(){
+    a.style.animationDuration=0.5+"s"
+}
+function fanon3(){
+    a.style.animationDuration=0.1+"s"
+}
